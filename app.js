ab074b223aec9058512041adf796f919952ff50a
Ahmet Furkan ÜNLÜ <63071548+af-unlu@users.noreply.github.com>
2022-02-21 00:38:56 +0300

    First 14 lines

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("test");
+//#region Init
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
+mongoose.connect(process.env.DB_BASE_URL, {useNewUrlParser: true});
+
+//#endregion
+
+
