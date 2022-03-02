38c655e15469b7584fdcf4c673dd5b5114661114
Ahmet Furkan ÜNLÜ <63071548+af-unlu@users.noreply.github.com>
2022-03-02 12:15:37 +0300

    auth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 const authController = require('../controllers/authController');
+const { checkUser } = require('../middleware/authMiddleware');
 
 const router = Router();
 
-router.get('/signup', authController.signup_get);
+router.get('/signup', checkUser ,authController.signup_get);
 router.post('/signup', authController.signup_post);
 router.get('/login', authController.login_get);
 router.post('/login', authController.login_post);
