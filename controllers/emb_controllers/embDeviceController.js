343e450c123b69fd28fc9534a6d2ebf714d2a8bf
af-unlu <ahmetfunlu@outlook.com>
2022-03-14 13:53:18 +0300

    Emb Routing & controllers links

---
+++ controllers/emb_controllers/embDeviceController.js
@@ -0,0 +1,57 @@
+//#region depends
+const User = require("../../models/User");
+const generateApiKey = require('generate-api-key');
+
+require('dotenv').config();
+
+const taskToDo= (req,res,task)=>{
+    if(res.locals.myStatus === 200){
+        if(res.locals.user._id.toString() == req.params.userId){
+            task();
+        }
+        else
+        {
+            res.status(403).json({"message":"403 Forbidden"});
+        }
+    }
+    else{
+        res.status(401).json({"message":"401 Unauthorized"});
+    }
+}  
+//#endregion
+
+/*
+checkUser => Logged User 
+URL Params : UserId
+Logged UserID and UserID  must match 
+*/
+//GET return all of ui pages of the user
+module.exports.get = async (req, res) => {
+    taskToDo(req,res,()=>{
+        res.status(200).json({"Page":"Get","userId":req.params.userId });
+    })
+}
+//PUT replaces all of ui's with request body if valid
+module.exports.update = async (req, res) => {
+    taskToDo(req,res,()=>{
+        res.status(201).json({"Page":"Put","userId":req.params.userId });
+    })
+}
+//DELETE deletes all of 
+module.exports.delete = async (req, res) => {
+    taskToDo(req,res,()=>{
+        res.status(200).json({"Page":"Delete","userId":req.params.userId });
+    })
+}
+//POST add one ui page created from request body if valid
+module.exports.create_child = async (req, res) => {
+    taskToDo(req,res,()=>{
+        res.status(201).json({"Page":"Post","userId":req.params.userId });
+    })
+}
+
+module.exports.hardconfig_get = async (req, res) => {
+    taskToDo(req,res,()=>{
+        res.status(200).json({"Page":"Get","userId":req.params.userId });
+    })
+}
--- routes/subRoutes/embRoutes.js
+++ routes/subRoutes/embRoutes.js
@@ -1,12 +1,17 @@
 const { Router } = require('express');
-const Controller = require('../../controllers/embController');
+const embController = require('../../controllers/emb_controllers/embController');
+const embDeviceController = require('../../controllers/emb_controllers/embDeviceController');
+const embCanController = require('../../controllers/emb_controllers/embCanController');
+const embCanMessageController = require('../../controllers/emb_controllers/embCanMessageController');
+const embUartController = require('../../controllers/emb_controllers/embUartController');
+const embUartMessageController = require('../../controllers/emb_controllers/embUartMessageController');
 const { checkUser } = require('../../middleware/authMiddleware');
 
 const router = Router();
 
-//Gömülü hardconfigi isteyecek
+
 router.route('/hardconfig/:apikey')
-.get(Controller.hardconfig_get);
+.get(embDeviceController.hardconfig_get);
 
 router.route('/test')
 .get(async (req, res) => {
@@ -16,20 +21,44 @@ router.route('/test')
 
 router.use(checkUser);
 
-//uis of an user
+//embs of an user
 router.route('/user=:userId')
-.get(Controller.get_all)                    
-.post(Controller.create_one)
-.put(Controller.update_all)
-.delete(Controller.delete_all);
+.get(embController.get)       //gets all embs      
+.put(embController.update)    //replaces all embs  
+.delete(embController.delete) //deletes all embs
+.post(embController.create_child);  //creates one emb
 
-//specific ui of an user
-router.route('/user=:userId/emb=:uiId')
-.get(Controller.get_one)              
-.post(async (req, res) => {res.status(405).json({ "Message":"Forbidden" });})
-.put(Controller.update_one)
-.delete(Controller.delete_one);
+//specific emb of an user
+router.route('/user=:userId/emb=:embId')
+.get(embDeviceController.get)       //get that emb       
+.put(embDeviceController.update)    //replace that emb
+.delete(embDeviceController.delete) //delete that emb
+.post(embDeviceController.create_child);//because there is 2 childs and these childs are not array its not allowed
 
+router.route('/user=:userId/emb=:embId/can')
+.get(embCanController.get)       //return can object       
+.put(embCanController.update)    //replace can object 
+.delete(embCanController.delete) //delete - emty can object
+.post(embCanController.create_child);//create can message
+
+router.route('/user=:userId/emb=:embId/can/canmessage=messageId')
+.get(embCanMessageController.get)       //get that can message       
+.put(embCanMessageController.update)    //replace that can message
+.delete(embCanMessageController.delete) //delete that can message
+.post(embCanMessageController.create_child);//not allowed
+
+
+router.route('/user=:userId/emb=:embId/uart')
+.get(embUartController.get)      //return uart object        
+.put(embUartController.update)   //replace uart object
+.delete(embUartController.delete)//delete - empty 
+.post(embUartController.create_child);//create new message-data  
+
+router.route('/user=:userId/emb=:embId/uart/data=dataId')
+.get(embUartMessageController.get)     //get that message         
+.put(embUartMessageController.update)  //replace that message 
+.delete(embUartMessageController.delete)//delete that message - maybe we need to adjust indexses again? 
+.post(embUartMessageController.create_child);//not allowed  
 
 
 module.exports = router;
