14aceed13e8ffd51b61c986258868c067ded4816
af-unlu <ahmetfunlu@outlook.com>
2022-03-19 22:31:19 +0300

    Looking for api key generation

--- controllers/deviceIdController.js
+++ controllers/deviceIdController.js
@@ -20,30 +20,25 @@ const taskToDo= (req,res,task)=>{
 }  
 //#endregion
 
-/*
-checkUser => Logged User 
-URL Params : UserId
-Logged UserID and UserID  must match 
-*/
-//GET return all of ui pages of the user
+
 module.exports.get = async (req, res) => {
     taskToDo(req,res,()=>{
         res.status(200).json({"Page":"Get","userId":req.params.userId });
     })
 }
-//PUT replaces all of ui's with request body if valid
+
 module.exports.update = async (req, res) => {
     taskToDo(req,res,()=>{
         res.status(201).json({"Page":"Put","userId":req.params.userId });
     })
 }
-//DELETE deletes all of 
+
 module.exports.delete = async (req, res) => {
     taskToDo(req,res,()=>{
         res.status(200).json({"Page":"Delete","userId":req.params.userId });
     })
 }
-//POST add one ui page created from request body if valid
+
 module.exports.create_child = async (req, res) => {
     taskToDo(req,res,()=>{
         res.status(201).json({"Page":"Post","userId":req.params.userId });
